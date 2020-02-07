de97bb257ded8692ebe4ee16232b34cbd315e789
Raychengole97 <raychengole97@gmail.com>
2020-02-07 03:40:16 +0300

    add heroku port in app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,17 +10,29 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
-    public static <Squads> void main(String[] args) {
-        ProcessBuilder processor = new ProcessBuilder();
-        int port;
+//    public static <Squads> void main(String[] args) {
+//        ProcessBuilder processor = new ProcessBuilder();
+//        int port;
+//
+//        if (processor.environment().get("PORT") != null) {
+//            port = Integer.parseInt(processor.environment().get("PORT"));
+//        } else {
+//            port = 4567;
+//        }
+//        port(port);
+//        staticFileLocation("/public");
+static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+}
+    public static void main(String[] args) {
 
-        if (processor.environment().get("PORT") != null) {
-            port = Integer.parseInt(processor.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-        port(port);
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
+
 
 
         get("/", (request, response) -> {
